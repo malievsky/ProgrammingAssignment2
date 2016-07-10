16d25567dc591acfda6821fdb2285ad61eaaf9d0
malievsky <malievsky@gmail.com>
2016-07-10 10:57:40 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Second program assignment
+## Usage of lexicaal scoping to cache results
 
-## Write a short comment describing this function
+## Function gets the matrix, calculates its 
+## inversion (OOTB function "solve") and caches 
+## result as variable "inv" in function(or global?)
+## envirnoment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) inv <<- solve
+  getInv <- function() inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+
+## function loads the variable "inv" from cache
+## fucntion checks whether exists
+## if yes - just reuses it
+## else - calculates it using OOTB "solve" function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)){
+    message("getting cached inversion")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInv(inv)
+  inv
 }
+
+
